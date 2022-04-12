dd6288bc929eda75ce4100f58c5a6f734e851d42
RobertoVida <roberto50005000@gmail.com>
2022-04-12 15:45:45 -0300

    CHECKOUT 1.1 (API) -- ter, 12 de abr de 2022 15:45:45

--- app/Http/Controllers/Checkout.php
+++ app/Http/Controllers/Checkout.php
@@ -5,9 +5,109 @@ namespace App\Http\Controllers;
 use App\Models\Carrinho;
 use Illuminate\Http\Request;
 
+use PagSeguro\Configuration\Configure;
+
 class Checkout extends Controller
 {
+    private $_configs;
+
+    public function __construct()
+    {
+        $this->_configs = new Configure();
+        $this->_configs->setCharset("UTF-8");
+        $this->_configs->setAccountCredentials(
+            env('PAGSEGURO_EMAIL'),
+            env('PAGSEGURO_TOKEN')
+        );
+        $this->_configs->setEnvironment(env('PAGSEGURO_AMBIENTE'));
+        $this->_configs->setLog(
+            true,
+            storage_path('logs/pagseguro_'.date('Ymd').'.log')
+        );
+    }
+
+    public function getCrt()
+    {
+        return $this->_configs->getAccountCredentials();
+    }
+
+
+    public function finalPayment(Request $req)
+    {
+        $idPedido = rand(2, 999);
+
+        $credCard = new \PagSeguro\Domains\Requests\DirectPayment\CreditCard();
+        $credCard->setReference("PED_".$idPedido);
+        $credCard->setCurrency("BRL");
+
+        $credCard->addItems()->withParameters(
+            $idPedido,
+            'blusa',
+            1,
+            number_format($req->valorinicio, 2, ".", "")
+        );
+
+        $user = $req->user_name::user(); 
+
+        $credCard->setSender()->setName($user->name. " ". $user->name);
+        $credCard->setSender()->setEmail("xerecard@sandbox.pagseguro.com.br");
+        $credCard->setSender()->setHash($req->hashseller);
+        $credCard->setSender()->setPhone()->withParameters(71, 87728789);
+        $credCard->setSender()->setDocument()->withParameters("CPF", "11111111111");
+
+        $credCard->setShipping()->setAddress()->withParameters(
+            'Av Optuco',
+            '1234',
+            'Vale do Silêncio',
+            '22775559',
+            'Goiânia',
+            'GO',
+            'BRA',
+            'terreo'
+        );
+        $credCard->setBilling()->setAddress()->withParameters(
+            'Av Optuco',
+            '1234',
+            'Vale do Silêncio',
+            '22775559',
+            'Goiânia',
+            'GO',
+            'BRA',
+            'terreo'
+        );
+        $credCard->setToken($req->cardtoken);
+        $credCard->setInstallment()->withParameters(
+            $req->numeroparcela,
+            $req->totalparcela,
+            2
+        );
+
+        $credCard->setHolder()->setName($user->name. " ". $user->name);
+        $credCard->setHolder()->setDocument()->withParameters("CPF", "11111111111");
+        $credCard->setHolder()->setBirthDate("03/01/1990");
+        $credCard->setHolder()->setPhone()->withParameters(71, 87728789);
+        $credCard->setMode("DEFAULT");
+
+        $result = $credCard->register($this->getCrt());
+
+        echo "TUDO CERTO";
+
+    }
     
+
+    // FORMULÁRIO DE PAGAMENTO (PROVISORIO)
+    public function postPayment(Request $req)
+    {
+        $data = [];
+        $sessionCode = \PagSeguro\Services\Session::create(
+            $this->getCrt()
+        );
+        $IDSession = $sessionCode->getResult();
+        $data["sessionID"] = $IDSession;
+    }
+
+
+
     // CARINHO DE COMPRA(TRAVADO)
     public function postCart(Request $req)
     {
@@ -24,14 +124,6 @@ class Checkout extends Controller
         $pix = $req->metodo_pix;
         $boleto = $req->metodo_boleto;
         $cartao = $req->metodo_cartao;
-    }
-
-
-
-    // FORMULÁRIO DE PAGAMENTO (PROVISORIO)
-    public function postPayment(Request $req)
-    {
-
     }
 
 
