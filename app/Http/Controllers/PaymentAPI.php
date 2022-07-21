2266ecdb712aa7d0d0e057e84882919dedb0114a
RobertoVida <roberto50005000@gmail.com>
2022-07-20 21:16:20 -0300

    qua, 20 de jul de 2022 21:16:20

---
+++ app/Http/Controllers/PaymentAPI.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentAPI extends Controller
+{
+    // CRIAR O CLIENTE NO ASSAS
+    public function createClient()
+    {
+
+    }
+
+    // LISTAR UM CLIENTE DO ASSAS
+    public function getOneClient()
+    {
+
+    }
+
+    // LISTAR CLIENTES DO ASSAS
+    public function listClient()
+    {
+
+    }
+
+    // PAGAR COM CARTÃO
+    public function payCard()
+    {
+
+    }
+
+    // PAGAR COM BOLETO
+    public function payBoleto()
+    {
+
+    }
+
+    // PAGAR COM PIX
+    public function payPix()
+    {
+
+    }
+
+    // PAGAR PARCELADO
+    public function payInstallment()
+    {
+
+    }
+
+    // PEGAR NÚMERO DIGITÁVEL DO BOLETO
+    public function getNumberLineBoleto()
+    {
+
+    }
+
+    // PAGAR PARCELADO
+    public function getQRcodePix()
+    {
+
+    }
+}
