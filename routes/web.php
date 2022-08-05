a0b4289c72c79e5396ef856b0a811c8ce549071a
RobertoVida <roberto50005000@gmail.com>
2022-08-05 14:25:51 -0300

    sex,  5 de ago de 2022 14:25:51

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ use Inertia\Inertia;
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->name('dashboard');
+
+// Route::get('/pera', function () {
+//     echo bcrypt('roberto123');
+// });
