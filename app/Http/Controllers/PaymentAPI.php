2a0eedc62e969d743f271664a971a31bb578cff8
RobertoVida <roberto50005000@gmail.com>
2023-01-27 23:34:03 -0300

    Fri, Jan 27, 2023 11:34:02 PM

--- app/Http/Controllers/PaymentAPI.php
+++ app/Http/Controllers/PaymentAPI.php
@@ -201,6 +201,7 @@ class PaymentAPI extends Controller
         $user = User::where("id", $req->id_user)->get();
         $adress = Endereco::where("fk_id_usuario", $req->id_user)->get();
         $info = InfoPessoais::where("fk_id_usuario", $req->id_user)->get();
+        return "debug";
         $desc = "PEDIDO User_".$verIdAsaas["id_asaas"].":  \n \n";
         for($i = 0; count($req->items); $i++)
         {
@@ -247,6 +248,12 @@ class PaymentAPI extends Controller
             $pagar = $this->requestAsaas("payments", $body, "POST");
             if($pagar && $pagar["success"] === true)
             {
+                // $status = Compras::where("id", $req->id_compra)
+                // ->update([
+                //     "status" => "Aguardando Pagamento",
+                //     "data_hora_compra" => date("Y-m-d H:i:s"),
+                //     "local_atual" => "No depósito"
+                // ]);
                 return [
                     "success" => true,
                     "data" => $pagar["data"]
